6ad8c20f8a3b4ed43574b243700d8e92404f2592
hashus42 <hsnhsyntrkmn@gmail.com>
2024-08-05 16:37:20 +0300

    mouse selection added

--- python-tracker/tracker.py
+++ python-tracker/tracker.py
@@ -1,12 +1,28 @@
 import os
 import cv2 as cv
+import numpy as np
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     raise IOError('Video could not be opened')
 
+detected = False
+gotBox = False
+detByCascade = False
+mouseDown = False
+mouseUp = False
+drawBox = np.zeros(4)
+start_x = 0
+start_y = 0
+initialized = False
+rois = ()
+trackers = []
+
 
 # relative path to exact path
+# When developer want to make this program an executable
+# relative paths would not work, this code returns exact paths of
+# relative paths for every program
 def resource_path(relative):
     return os.path.join(
         os.environ.get(
@@ -17,61 +33,97 @@ def resource_path(relative):
     )
 
 
-detected = False
-success = False
+def on_mouse(event, x, y, flags, param):
+    global mouseDown, mouseUp, start_x, start_y, roi, initialized, drawBox
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        mouseDown = True
+        mouseUp = False
+        start_x = x
+        start_y = y
+        drawBox[0] = start_x
+        drawBox[1] = start_y
+    elif mouseDown and event == cv.EVENT_MOUSEMOVE:
+        if x > start_x:
+            drawBox[2] = x - start_x
+        else:
+            drawBox[2] = start_x - x
+            drawBox[0] = x
+        if y > start_y:
+            drawBox[3] = y - start_y
+        else:
+            drawBox[3] = start_y - y
+            drawBox[1] = y
+    elif event == cv.EVENT_LBUTTONUP:
+        mouseDown = False
+        mouseUp = True
+        initialized = True
+
+
 while True:
     _, frame = cap.read()
     frame = cv.flip(frame, 1)
+    frame_process = frame.copy()
     start = cv.getTickCount()
-
+    # This should be run in every bunch of second
     if not detected:
         if frame is not None:
-            frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            frame_gray = cv.cvtColor(frame_process, cv.COLOR_BGR2GRAY)
+            frame_rgb = cv.cvtColor(frame_process, cv.COLOR_BGR2RGB)
             stop_haarcascade = cv.CascadeClassifier(resource_path('stop_data.xml'))
-            roi = stop_haarcascade.detectMultiScale(frame_gray, 1.3, 5)
-
-            # roi = cv.selectROI("tracker", frame)
+            detectedBox = stop_haarcascade.detectMultiScale(frame_gray, 1.3, 5)
+            # if haar cascade detect any thing
+            if len(detectedBox) > 0:
+                roi = detectedBox
+                initialized = True
+                detected = True
+                detByCascade = True
         else:
             raise ValueError("Error: Frame is empty or invalid")
 
-        amount_found = len(roi)
-
-        if amount_found != 0:
+    if initialized:
+        if detByCascade:
             roi = tuple(roi.flatten())
-            rois = tuple(roi[i:i+4] for i in range(0, len(roi), 4))
-            try:
-                trackers = []
-                for i in range(len(rois)):
-                    roi = rois[i]
-                    print(roi)
-                    tracker = cv.TrackerCSRT_create()
-                    trackers.append(tracker)
-                    trackers[i].init(frame, roi)
-                detected = True
-            except Exception as e:
-                print(e)
+            rois += tuple(roi[i:i + 4] for i in range(0, len(roi), 4))
+            gotBox = True
+            detByCascade = False
+        else:
+            roi = tuple(drawBox.flatten())
+            rois = rois + (roi,)
+            gotBox = True
+        try:
+            for i in range(len(rois)):
+                roi = rois[i]
+                tracker = cv.TrackerCSRT_create()
+                trackers.append(tracker)
+                trackers[i].init(frame_process, roi)
+        except Exception as e:
+            print(e)
+        initialized = False
 
-    elif detected:
+    if gotBox:
         for i in range(len(rois)):
             roi = rois[i]
-            success, roi = trackers[i].update(frame)
+            _, roi = trackers[i].update(frame_process)
             roi = tuple(int(i) for i in roi)
-            if success:
-                cv.rectangle(frame, roi, (0, 255, 0), 2)
-            else:
-                # detected = False
-                pass
+            x, y, w, h = roi
+            # If the detected image get smaller its label also should get smaller
+            # that is why we give font scale w/100. Mean w is 100 and font scale is 1
+            # if w become like 33 the font scale will be 0.3
+            cv.putText(frame, f"object {i}", (x, y-10),
+                       cv.FONT_HERSHEY_SIMPLEX, w/125, (0, 200, 0), 2)
+            cv.rectangle(frame, roi, (0, 255, 0), 2)
 
     fps = cv.getTickFrequency() / (cv.getTickCount() - start)
 
     # show pfs
     cv.putText(frame, f"FPS: {round(fps)}", (10, 30),
                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-    cv.putText(frame, "Detected" if success else "No Detection", (10, 50),
-               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0) if success else (255, 0, 0))
+    cv.putText(frame, "Detected" if detected else "No Detection", (10, 50),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0) if detected else (255, 0, 0))
 
     cv.imshow("tracker", frame)
+    cv.setMouseCallback("tracker", on_mouse)
 
     key = cv.waitKey(1)
     if key == ord('q') or key == 27:
